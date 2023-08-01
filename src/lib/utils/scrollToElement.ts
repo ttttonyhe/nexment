bb2d1891a89d36f01fd332c8db2671eab53db6ce
Tony He <tony.hlp@hotmail.com>
2023-08-01 02:04:56 -0400

    fix: scroll into view logic

--- src/lib/utils/scrollToElement.ts
+++ src/lib/utils/scrollToElement.ts
@@ -1,5 +1,18 @@
+const getOffsetTop = (element: any) => {
+	let offsetTop = 0
+	while (element) {
+		offsetTop += element.offsetTop
+		element = element.offsetParent
+	}
+	return offsetTop
+}
+
 export const scrollToElementById = (id: string) => {
-	const commentAreaOffsetTop = document.getElementById(id)?.offsetTop || 100
+	const element = document.getElementById(id)
+	if (!element) return
+
+	const commentAreaOffsetTop = getOffsetTop(document.getElementById(id)) || 100
+
 	window.scrollTo({
 		top: commentAreaOffsetTop - 100,
 		behavior: "smooth",
