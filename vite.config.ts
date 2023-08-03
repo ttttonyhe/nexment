651adb11a90a809eaba724b3fb98cea0292e5fb2
Tony He <tony.hlp@hotmail.com>
2023-08-03 18:37:52 -0400

    chore: update build config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc"
 
 export default defineConfig({
 	build: {
-		minify: false,
+		minify: true,
 		rollupOptions: {
 			input: {
 				main: "./index.html",
