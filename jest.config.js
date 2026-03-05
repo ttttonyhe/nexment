9b02d5c0e083089d416e82422390ccc4c865c3cd
Tony L. He <tony.hlp@hotmail.com>
2026-03-05 10:53:52 -0500

    fix: ci errors

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
-import type { Config } from "jest"
-
-const config: Config = {
+/** @type {import('jest').Config} */
+module.exports = {
 	testEnvironment: "jsdom",
 	roots: ["<rootDir>/__tests__"],
 	transform: {
@@ -18,5 +17,3 @@ const config: Config = {
 	setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 	testMatch: ["**/*.test.ts", "**/*.test.tsx"],
 }
-
-export default config
